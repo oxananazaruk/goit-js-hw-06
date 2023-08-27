9922e3ee5cf6936157fcaf79e62c8f90f588c297
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-08-27 08:38:51 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+const body = document.querySelector('body');
+
+
+
+function onButtonChangeColorHandler() {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+  colorValue.textContent = `${randomColor}`
+}
+
+buttonChangeColor.addEventListener('click', onButtonChangeColorHandler);
