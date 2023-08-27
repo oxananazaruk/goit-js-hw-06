62384db5a4bcb395cc32f664d4f894ec13f20b7c
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-08-27 08:26:11 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formLogin = document.querySelector('.login-form');
 
+// console.dir(formLogin);
+
+function onFormSubmitHandler(event) {
+    event.preventDefault();
+
+    const loginValue = event.currentTarget.elements.email.value;
+    const passwordValue = event.currentTarget.elements.password.value;
+
+    if (loginValue === "" || passwordValue === "") {
+        return alert('All form fields must be filled!')
+    };
+
+    const result = {
+       email: loginValue,
+        password: passwordValue,
+    };
+
+    console.log(result);
+
+    event.currentTarget.reset();
+};
+
+formLogin.addEventListener('submit', onFormSubmitHandler);
